e13eae698e17039192bbf3692c9608d21c8b0142
Alireza Gharib <alirezagharib110@gmail.com>
2024-08-05 20:29:57 +0330

    Update setup_index.sh

--- setup_index.sh
+++ setup_index.sh
@@ -12,7 +12,7 @@ export $(cat .env | grep -v ^# | xargs)
 for YEAR in {2018..2030}; do
   for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12; do
     
-    curl --cacert ${ES_CA_CERTS} --user ${ES_USERNAME}:${ES_PASSWORD} \
+    curl \
     -XPUT "${ES_URL}tor-$YEAR-$MONTH/" \
     -H 'Content-Type: application/json' -d "@./mappings_tor.json"
 
